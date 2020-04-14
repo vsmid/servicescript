1e1b30dab4b70bd846c92b91b017149ac22e5bec
Vedran Smid <vedransmid@yeti-it.hr>
2020-04-14 11:10:37 +0200

    Initial commit.

---
+++ CarsService.groovy
@@ -0,0 +1,45 @@
+import com.sun.net.httpserver.Authenticator
+import com.sun.net.httpserver.Filter
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpPrincipal
+
+import static ServiceScript.*
+
+int callCounter = 0
+
+Map cars = [
+        "Mazda": ["Mazda 3", "Mazda 6"], 
+        "Suzuki": ["Swift", "Jimmy"]
+]
+
+BasicAuth basicAuth = { String username, String password ->  username == "lena" && password == "123" }
+
+Middleware requestCounter = [
+        doFilter: { HttpExchange exchange, Filter.Chain chain ->
+            println "${exchange.requestURI} called ${++callCounter} time(s)"
+            chain.doFilter exchange
+        }
+]
+
+Method findAll = [
+        name      : "findAll",
+        exchange  : { HttpExchange exchange -> exchange.json 200, cars },
+        middleware: [ requestCounter ]
+]
+
+Method findOne = [
+        name      : "findOne",
+        exchange  : { HttpExchange exchange -> 
+                println "Authenticatd user: ${exchange.principal}"
+                exchange.json 200, cars[exchange.jsondata()?.type] ?: []
+        },
+        middleware: [ requestCounter ],
+        authenticator: basicAuth
+]
+
+Method metrics = [
+        name    : "metrics",
+        exchange: { HttpExchange exchange -> exchange.json 200, ["metrics": ["calls": callCounter]] }
+]
+
+expose 1111, findAll, findOne, metrics
---
+++ ServiceScript.groovy
@@ -0,0 +1,75 @@
+import com.sun.net.httpserver.*
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
+class ServiceScript {
+
+    private ServiceScript() {}
+
+    static JsonSlurper JSON = new JsonSlurper()
+
+    static abstract class BasicAuth extends BasicAuthenticator {
+        BasicAuth() {
+            super("basic")
+        }
+    }
+
+    static class Method {
+        String name
+        Closure exchange
+        List<Middleware> middleware
+        Authenticator authenticator
+    }
+
+    static class Middleware extends Filter {
+        Closure doFilter
+
+        @Override
+        void doFilter(HttpExchange exchange, Chain chain) throws IOException {
+            this.doFilter.call exchange, chain
+        }
+
+        @Override
+        String description() {
+            ""
+        }
+    }
+
+    static void expose(int port = new Random().nextInt(9000 - 5000 + 1) + 5000, Method... methods) {
+        extend()
+
+        HttpServer server
+        server = HttpServer.create new InetSocketAddress(port), 0
+
+        methods.each {
+            HttpContext context = server.createContext "/" + it.name, it.exchange as HttpHandler
+            if (it?.authenticator) context.setAuthenticator it.authenticator
+            if (it?.middleware) context.filters.addAll it.middleware
+        }
+
+        server.setExecutor(null)
+
+        println "[info] Available service methods:"
+        println methods.collect { "/" + it.name }.join(System.lineSeparator())
+
+        server.start()
+
+        println "[info] Service opened on port ${port}"
+
+        server
+    }
+
+    static void extend() {
+        HttpExchange.metaClass {
+            out { status = 200, contentType = "*/*", content = "".bytes ->
+                delegate.responseHeaders.add "Content-type", contentType
+                delegate.sendResponseHeaders status, content.length
+                delegate.responseBody.write content
+                delegate.close()
+            }
+            json { status = 200, content -> delegate.out status, "application/json", JsonOutput.toJson(content).bytes }
+            jsondata { JSON.parse delegate.requestBody }
+            textdata { new String(delegate.requestBody.readAllBytes()) }
+        }
+    }
+}
