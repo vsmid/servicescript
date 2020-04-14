da14f9ada7d29af5d0d067ff5bf086d937fa70e9
Vedran Smid <vedransmid@yeti-it.hr>
2020-04-14 16:42:01 +0200

    Code formatting.

--- ServiceScript.groovy
+++ ServiceScript.groovy
@@ -10,7 +10,45 @@ class ServiceScript {
     static JsonSlurper JSON = new JsonSlurper()
     static HttpServer HTTP_SERVER
 
+    static {
+        extend()
+    }
+
     private ServiceScript() {}
+
+    static void expose(int port = new Random().nextInt(9000 - 5000 + 1) + 5000, Method... methods) {
+        if (!HTTP_SERVER) {
+            HTTP_SERVER = HttpServer.create new InetSocketAddress(port), 10
+            HTTP_SERVER.setExecutor Executors.newFixedThreadPool(25)
+        }
+
+        methods.each {
+            HttpContext context = HTTP_SERVER.createContext "/" + it.name, new ExchangeHandler(it.exchange)
+            if (it?.authenticator) context.setAuthenticator it.authenticator
+            if (it?.middleware) context.filters.addAll it.middleware
+        }
+
+        println "[info] Available service methods:"
+        println methods.collect { "/" + it.name }.join(System.lineSeparator())
+
+        HTTP_SERVER.start()
+
+        println "[info] Service opened on port ${HTTP_SERVER.address.port}"
+    }
+
+    static void extend() {
+        HttpExchange.metaClass {
+            out { status = 200, contentType = "*/*", content = "".bytes ->
+                delegate.responseHeaders.add "Content-type", contentType
+                delegate.sendResponseHeaders status, content.length
+                delegate.responseBody.write content
+                delegate.close()
+            }
+            json { status = 200, content -> delegate.out status, "application/json", JsonOutput.toJson(content).bytes }
+            jsondata { JSON.parse delegate.requestBody }
+            textdata { new String(delegate.requestBody.readAllBytes()) }
+        }
+    }
 
     static abstract class BasicAuth extends BasicAuthenticator {
         BasicAuth() {
@@ -51,42 +89,6 @@ class ServiceScript {
         @Override
         String description() {
             ""
-        }
-    }
-
-    static void expose(int port = new Random().nextInt(9000 - 5000 + 1) + 5000, Method... methods) {
-        extend()
-
-        if (!HTTP_SERVER) {
-            HTTP_SERVER = HttpServer.create new InetSocketAddress(port), 10
-            HTTP_SERVER.setExecutor Executors.newFixedThreadPool(25)
-        }
-
-        methods.each {
-            HttpContext context = HTTP_SERVER.createContext "/" + it.name, new ExchangeHandler(it.exchange)
-            if (it?.authenticator) context.setAuthenticator it.authenticator
-            if (it?.middleware) context.filters.addAll it.middleware
-        }
-
-        println "[info] Available service methods:"
-        println methods.collect { "/" + it.name }.join(System.lineSeparator())
-
-        HTTP_SERVER.start()
-
-        println "[info] Service opened on port ${HTTP_SERVER.address.port}"
-    }
-
-    static void extend() {
-        HttpExchange.metaClass {
-            out { status = 200, contentType = "*/*", content = "".bytes ->
-                delegate.responseHeaders.add "Content-type", contentType
-                delegate.sendResponseHeaders status, content.length
-                delegate.responseBody.write content
-                delegate.close()
-            }
-            json { status = 200, content -> delegate.out status, "application/json", JsonOutput.toJson(content).bytes }
-            jsondata { JSON.parse delegate.requestBody }
-            textdata { new String(delegate.requestBody.readAllBytes()) }
         }
     }
 }
